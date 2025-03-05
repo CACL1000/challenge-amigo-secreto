d7ade708a81e0598c777a257d41e4a4349d3ebe6
Camilo Leon <leoncamilo.dev@gmail.com>
2025-03-05 11:13:02 -0500

    agrega funcion para añadir amigos y actualiza la lista en pantalla

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,34 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Array para almacenar los nombres de los amigos
+let listaDeAmigos = [];
+
+function agregarAmigo() {
+    
+    let inputNombre = document.getElementById("amigo");
+    let nombre = inputNombre.value.trim(); // Trim para eliminar espacios en blanco al inicio y final
+
+    if (nombre === "") {
+        alert("Por favor, inserte un nombre.");
+        return; 
+    }
+
+    listaDeAmigos.push(nombre);
+    actualizarListaAmigos();
+    inputNombre.value = "";
+}
+
+function actualizarListaAmigos() {
+    let listaHTML = document.getElementById("listaAmigos");
+    listaHTML.innerHTML = ""; // Limpiar la lista antes de actualizar
+
+    // Recorrer el array y agregar cada nombre a la lista en el HTML
+    listaDeAmigos.forEach((amigo) => {
+        let li = document.createElement("li");
+        li.textContent = amigo;
+        listaHTML.appendChild(li);
+    });
+}
+
+
+
+
+
