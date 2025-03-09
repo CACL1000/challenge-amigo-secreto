fe93abc081da1159f2fa84865ec8f3f9efeb8c05
Camilo Leon <leoncamilo.dev@gmail.com>
2025-03-09 10:57:33 -0500

    agrega funcion de sortear amigo

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -2,33 +2,46 @@
 let listaDeAmigos = [];
 
 function agregarAmigo() {
-    
-    let inputNombre = document.getElementById("amigo");
-    let nombre = inputNombre.value.trim(); // Trim para eliminar espacios en blanco al inicio y final
+  let inputNombre = document.getElementById("amigo");
+  let nombre = inputNombre.value.trim(); // Trim para eliminar espacios en blanco al inicio y final
 
-    if (nombre === "") {
-        alert("Por favor, inserte un nombre.");
-        return; 
-    }
+  if (nombre === "") {
+    alert("Por favor, inserte un nombre.");
+    return;
+  }
 
-    listaDeAmigos.push(nombre);
-    actualizarListaAmigos();
-    inputNombre.value = "";
+  listaDeAmigos.push(nombre);
+  actualizarListaAmigos();
+  inputNombre.value = "";
 }
 
 function actualizarListaAmigos() {
-    let listaHTML = document.getElementById("listaAmigos");
-    listaHTML.innerHTML = ""; // Limpiar la lista antes de actualizar
+  let listaHTML = document.getElementById("listaAmigos");
+  listaHTML.innerHTML = ""; // Limpiar la lista antes de actualizar
 
-    // Recorrer el array y agregar cada nombre a la lista en el HTML
-    listaDeAmigos.forEach((amigo) => {
-        let li = document.createElement("li");
-        li.textContent = amigo;
-        listaHTML.appendChild(li);
-    });
+  // Recorrer el array y agregar cada nombre a la lista en el HTML
+  listaDeAmigos.forEach((amigo) => {
+    let li = document.createElement("li");
+    li.textContent = amigo;
+    listaHTML.appendChild(li);
+  });
 }
 
+function sortearAmigo() {
 
+  if (listaDeAmigos.length === 0) {
+    alert("No hay amigos para sortear");
+    return;
+  }
 
+  //generar indice aleatorio
+  let indiceAleatorio = Math.floor(Math.random() * listaDeAmigos.length);
 
+  //obtener amigo sorteado  
+  let amigoSorteado = listaDeAmigos[indiceAleatorio];
+  console.log("Amigo sorteado: ", amigoSorteado);
 
+  //mostar resultado en la pagina
+  let resultado = document.getElementById("resultado");
+  resultado.innerHTML = `<li>${amigoSorteado}</li>`;
+}
